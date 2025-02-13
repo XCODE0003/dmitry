3f1229d1c1ce3ccc392a2b6a0d30be303acd1e8a
work <nikita@Air-Nikita.domrurouter.net>
2025-02-13 13:51:39 +0500

    last

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -6,7 +6,7 @@ const userStore = useUserStore();
 const systemStore = useSystemStore();
 
 onMounted(async () => {
-    const userId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id || 7084589048;
+    const userId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id;
     if (userId) {
         await userStore.fetchUser(userId);
         await systemStore.fetchDeals();
