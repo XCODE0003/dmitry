3e4edde9f6f823dc31da298f0539ecdd83aaadd3
work <nikita@MacBook-Air-Nikita.local>
2025-03-08 15:04:06 +0500

    last

--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -284,8 +284,9 @@ function checkDeal(bundle) {
                 <div v-if="timeLeft(deal) !== 'ended' && deal.type === 'fixed'" class="p-2 bg-gray-300 rounded-lg text-sm w-fit">До конца работы:
                     {{ timeLeft(deal) }}</div>
                 <button @click="userStore.withdraw(deal.id)" v-if="timeLeft(deal) === 'ended'"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Забрать
-                    прибыль</button>
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
+                    {{ deal.type === 'fixed' ? 'Забрать прибыль' : 'Закрыть депозит' }}
+                </button>
             </div>
         </div>
         <section v-if="systemStore.activeTab === 'work' && systemStore?.deals.length === 0"
